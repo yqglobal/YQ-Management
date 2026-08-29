233b465aeeaab7ba3eb8a554ffbbcdabd19b763d
Abhimanyu Negi <avi2507.work@gmail.com>
2026-08-29 15:21:37 +0530

    fix: configure websocket cors

--- YQ Management/backend/src/redis/redis-io.adapter.ts
+++ YQ Management/backend/src/redis/redis-io.adapter.ts
@@ -17,6 +17,13 @@ export class RedisIoAdapter extends IoAdapter {
   }
 
   createIOServer(port: number, options?: ServerOptions): any {
+    options = {
+      ...options,
+      cors: {
+        origin: true,
+        credentials: true,
+      },
+    };
     const server = super.createIOServer(port, options);
     server.adapter(this.adapterConstructor);
     return server;
